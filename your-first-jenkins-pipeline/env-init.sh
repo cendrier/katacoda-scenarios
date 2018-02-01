6a25428a2e1e77c9d021085e765d6730d3f9f0f0
Damien DUPORTAL <damien.duportal@gmail.com>
2018-02-01 14:32:51 +0100

    wip first experiment
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>

--- your-first-jenkins-pipeline/env-init.sh
+++ your-first-jenkins-pipeline/env-init.sh
@@ -8,3 +8,6 @@ DOCKER_COMPOSE_VERSION=1.18.0
 DOCKER_COMPOSE_BIN="$(which docker-compose)"
 curl -sSL -o "${DOCKER_COMPOSE_BIN}" \
   https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-"$(uname -s)"-"$(uname -m)"
+
+
+ls -al
