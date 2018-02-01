cdb41724606f924df0faf4d52c7260033317223b
Damien DUPORTAL <damien.duportal@gmail.com>
2018-02-01 16:51:15 +0100

    wip first experiment
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>

--- your-first-jenkins-pipeline/env-init.sh
+++ your-first-jenkins-pipeline/env-init.sh
@@ -3,11 +3,11 @@
 set -eu -o pipefail
 
 ## Install Docker-Compose
-echo "== Installing Docker-Compose"
-DOCKER_COMPOSE_VERSION=1.18.0
-DOCKER_COMPOSE_BIN="$(which docker-compose)"
-curl -sSL -o "${DOCKER_COMPOSE_BIN}" \
-  https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-"$(uname -s)"-"$(uname -m)"
+# echo "== Installing Docker-Compose"
+# DOCKER_COMPOSE_VERSION=1.18.0
+# DOCKER_COMPOSE_BIN="$(which docker-compose)"
+# curl -sSL -o "${DOCKER_COMPOSE_BIN}" \
+#   https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-"$(uname -s)"-"$(uname -m)"
 
 ## Get infra code
 git clone https://github.com/dduportal/katacoda-scenarios.git /tmp/repo
@@ -15,7 +15,7 @@ cp -r /tmp/repo/your-first-jenkins-pipeline/docker-compose.yml ~/
 cp -r /tmp/repo/your-first-jenkins-pipeline/.env ~/
 
 ## Start infra
-cd ~/
-env | sort
-docker-compose pull --parallel
-docker-compose up -d
+# cd ~/
+# env | sort
+# docker-compose pull --parallel
+# docker-compose up -d
