8573cc770803dfe966f9a075f2f6a836a7c1ea9f
Damien DUPORTAL <damien.duportal@gmail.com>
2018-02-01 20:06:31 +0100

    wip first experiment
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>

--- your-first-jenkins-pipeline/env-init.sh
+++ your-first-jenkins-pipeline/env-init.sh
@@ -10,7 +10,7 @@ curl -sSL -o "${DOCKER_COMPOSE_BIN}" \
   https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-"$(uname -s)"-"$(uname -m)"
 
 ## Start infra
-git clone https://github.com/dduportal/katacodas-scenarios.git /tmp/repo
+git clone https://github.com/dduportal/katacoda-scenarios.git /tmp/repo
 cp -r /tmp/repo/your-first-jenkins-pipeline/docker ~/
 cd ~/docker
 docker-compose pull --parallel
