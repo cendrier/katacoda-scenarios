d608da63fdee205fb2915350b940ccf136e4f046
Damien DUPORTAL <damien.duportal@gmail.com>
2018-02-01 17:22:18 +0100

    wip first experiment
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>

--- your-first-jenkins-pipeline/env-init.sh
+++ your-first-jenkins-pipeline/env-init.sh
@@ -10,5 +10,5 @@ curl -sSL -o "${DOCKER_COMPOSE_BIN}" \
   https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-"$(uname -s)"-"$(uname -m)"
 
 ## Start infra
-cd ~/
+cd ~/docker
 docker-compose pull --parallel
