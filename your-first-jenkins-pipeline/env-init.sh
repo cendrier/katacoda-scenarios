751fd16c774352d552ba2054c42887d948a91362
Damien DUPORTAL <damien.duportal@gmail.com>
2018-02-01 11:49:10 +0100

    wip first experiment
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>

--- your-first-jenkins-pipeline/env-init.sh
+++ your-first-jenkins-pipeline/env-init.sh
@@ -1,19 +1,10 @@
-#!/bin/sh
-
-set -eu -o pipefail
-
-## Install Docker-Compose
+git clone https://github.com/oufti-playground/lab-vm.git
+cd lab-vm/docker || :
+export DOCKER_USERNAME=dduportal
 DOCKER_COMPOSE_VERSION=1.18.0
 DOCKER_COMPOSE_BIN="$(which docker-compose)"
 
 curl -sSL -o "${DOCKER_COMPOSE_BIN}" \
   https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-"$(uname -s)"-"$(uname -m)"
-
-
-## Get Code
-git clone https://github.com/oufti-playground/lab-vm.git
-cd lab-vm/docker || :
-export DOCKER_USERNAME=dduportal
-
 docker-compose pull --parallel
 docker-compose up -d
