9b82a3266fac5f1f6f6dd1ddaf0b6f1cff70c549
Damien DUPORTAL <damien.duportal@gmail.com>
2018-02-01 17:20:39 +0100

    wip first experiment
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>

--- your-first-jenkins-pipeline/env-init.sh
+++ your-first-jenkins-pipeline/env-init.sh
@@ -9,13 +9,6 @@ DOCKER_COMPOSE_BIN="$(which docker-compose)"
 curl -sSL -o "${DOCKER_COMPOSE_BIN}" \
   https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-"$(uname -s)"-"$(uname -m)"
 
-## Get infra code
-git clone https://github.com/dduportal/katacoda-scenarios.git /tmp/repo
-cp -r /tmp/repo/your-first-jenkins-pipeline/docker-compose.yml ~/
-cp -r /tmp/repo/your-first-jenkins-pipeline/.env ~/
-
 ## Start infra
-# cd ~/
-# env | sort
-# docker-compose pull --parallel
-# docker-compose up -d
+cd ~/
+docker-compose pull --parallel
