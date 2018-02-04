790175330ec0d9e0459b77760a7526af7f301d6f
Damien DUPORTAL <damien.duportal@gmail.com>
2018-02-04 19:58:01 +0100

    WiP
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>

--- your-first-jenkins-pipeline/env-init.sh
+++ your-first-jenkins-pipeline/env-init.sh
@@ -9,7 +9,8 @@ docker run \
   -u root \
   -p 8080:8080 \
   -e JAVA_OPTS="-Djenkins.install.runSetupWizard=false -Dhudson.TcpSlaveAgentListener.hostName=localhost -Xms256m -Xmx512m" \
-  # -v "${HOME}/preconfigureJenkins.groovy":/var/jenkins_home/init.groovy.d/preconfigureJenkins.groovy \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v "${HOME}/git":/git \
   jenkinsci/blueocean
+
+  # -v "${HOME}/preconfigureJenkins.groovy":/var/jenkins_home/init.groovy.d/preconfigureJenkins.groovy \
