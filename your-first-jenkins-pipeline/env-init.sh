57d48c194cda3a9a360fc6a3b7d53d32017e1c49
Damien DUPORTAL <damien.duportal@gmail.com>
2018-02-04 19:34:08 +0100

    WiP
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>

--- your-first-jenkins-pipeline/env-init.sh
+++ your-first-jenkins-pipeline/env-init.sh
@@ -1,17 +1,2 @@
-#!/bin/sh
-
-# set -eu -o pipefail
-
-## Install Docker-Compose
-# echo "== Installing Docker-Compose"
-# DOCKER_COMPOSE_VERSION=1.18.0
-# DOCKER_COMPOSE_BIN="$(which docker-compose)"
-# curl -sSL -o "${DOCKER_COMPOSE_BIN}" \
-#   https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-"$(uname -s)"-"$(uname -m)"
-#
-# ## Start infra
-# git clone https://github.com/dduportal/katacoda-scenarios.git /tmp/repo
-# cp -r /tmp/repo/your-first-jenkins-pipeline/docker ~/
-# cd ~/docker
-# docker-compose pull --parallel
-# docker-compose up -d
+#!/bin/bash
+echo "$(date)" >> /tmp/token
